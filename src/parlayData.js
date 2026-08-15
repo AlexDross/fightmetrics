c6a9246448ac1d3d35644aac343fe4f8833cc51f
Alex Drossman <asdross@umich.edu>
2026-08-15 15:51:52 -0400

    Add new parlay entries for UFC events

--- src/parlayData.js
+++ src/parlayData.js
@@ -1,5 +1,73 @@
 export const PARLAY_ENTRIES = [
   {
+    "id": "1786823321443-sn35hb",
+    "createdAt": "2026-08-15T19:48:41.443Z",
+    "pickSource": "human",
+    "eventName": "UFC 330",
+    "eventDate": "2026-08-15",
+    "legs": [
+      {
+        "fightId": "1786823094478-i6q6sa",
+        "fighterA": "Mansur Abdul-Malik",
+        "fighterB": "Dustin Stoltzfus",
+        "eventName": "UFC 330",
+        "eventDate": "2026-08-15",
+        "pickedFighter": "Mansur Abdul-Malik",
+        "v2DefaultFighter": "Mansur Abdul-Malik",
+        "v2ProbAtBuild": 0.6437880074207147,
+        "overridden": false
+      },
+      {
+        "fightId": "1786823062490-fbmbbj",
+        "fighterA": "Edson Barboza",
+        "fighterB": "Esteban Ribovics",
+        "eventName": "UFC 330",
+        "eventDate": "2026-08-15",
+        "pickedFighter": "Esteban Ribovics",
+        "v2DefaultFighter": "Esteban Ribovics",
+        "v2ProbAtBuild": 0.6476034299370197,
+        "overridden": false
+      },
+      {
+        "fightId": "1786823040114-fo1k6b",
+        "fighterA": "Chidi Njokuani",
+        "fighterB": "Joel Alvarez",
+        "eventName": "UFC 330",
+        "eventDate": "2026-08-15",
+        "pickedFighter": "Joel Alvarez",
+        "v2DefaultFighter": "Joel Alvarez",
+        "v2ProbAtBuild": 0.6810217994372325,
+        "overridden": false
+      },
+      {
+        "fightId": "1786823004768-lj5jjp",
+        "fighterA": "Donte Johnson",
+        "fighterB": "Eric McConico",
+        "eventName": "UFC 330",
+        "eventDate": "2026-08-15",
+        "pickedFighter": "Donte Johnson",
+        "v2DefaultFighter": "Donte Johnson",
+        "v2ProbAtBuild": 0.6947343156308464,
+        "overridden": false
+      },
+      {
+        "fightId": "1786822892840-k2dfrm",
+        "fighterA": "Jeremiah Wells",
+        "fighterB": "Myktybek Orolbai",
+        "eventName": "UFC 330",
+        "eventDate": "2026-08-15",
+        "pickedFighter": "Myktybek Orolbai",
+        "v2DefaultFighter": "Myktybek Orolbai",
+        "v2ProbAtBuild": 0.791685136914412,
+        "overridden": false
+      }
+    ],
+    "combinedOdds": "+139",
+    "unitsWagered": 1,
+    "status": "PENDING",
+    "result": null
+  },
+  {
     "id": "1785450005324-99loag",
     "createdAt": "2026-07-30T22:20:05.324Z",
     "pickSource": "human",
